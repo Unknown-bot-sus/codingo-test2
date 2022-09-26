bf4c24203ed5c5fe4062d29571384a78a2e258b9
Thar Lin Shwe <albent2017@gmail.com>
2022-09-26 09:39:19 +0630

    Added Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from "@/styles/footer.module.scss";
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <span>Copyright &copy; Codigo - Mobile App Developer Singapore</span>
+    </footer>
+  );
+};
