48466b36af483832e60364ae7deb67a61a271235
Thar Lin Shwe <albent2017@gmail.com>
2022-09-26 09:41:40 +0630

    Added Team Cards & Player Cards to Dashboard

---
+++ components/TeamCard.tsx
@@ -0,0 +1,10 @@
+import { ITeam } from "@/utils/interfaces";
+import { FC } from "react";
+
+interface Props {
+  team: ITeam;
+}
+
+export const TeamCard: FC<Props> = ({ team }) => {
+  return <div></div>;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,52 @@ import Head from "next/head";
 import { NextPageWithLayout } from "types/page";
 import Layout from "@/layouts/.";
 import { get } from "@/utils/api";
-import { getTeams, setTeams } from "@/store/features/teamSlice";
-import { useSelector } from "react-redux";
+import {
+  getPlayers,
+  getTeams,
+  setPlayers,
+  setTeams,
+} from "@/store/features/teamSlice";
+import { useDispatch, useSelector } from "react-redux";
 import { wrapper } from "@/store/store";
+import { PlayerCard } from "@/components/PlayerCard";
+import { IPlayer, ITeam } from "@/utils/interfaces";
+
+import styles from "@/styles/index.module.scss";
+import { TeamCard } from "@/components/TeamCard";
 
 interface Props {}
 
 const Home: NextPageWithLayout<Props> = () => {
+  const dispatch = useDispatch();
+  const players = useSelector(getPlayers);
   const teams = useSelector(getTeams);
+
+  async function fetchPlayers() {
+    const res = await get(`players?per_page=10`);
+  }
 
   return (
     <div>
       <Head>
         <title>Dashboard</title>
       </Head>
-      <main></main>
+      <main className={styles.container}>
+        <div className={styles["card-container"]}>
+          <h2>Players</h2>
+          {players.map((player: IPlayer) => (
+            <PlayerCard player={player}></PlayerCard>
+          ))}
+        </div>
+        <div className={styles["card-container"]}>
+          <h2>Teams</h2>
+          {teams.length === 0 ? (
+            <span>No Teams</span>
+          ) : (
+            teams.map((team: ITeam) => <TeamCard team={team} />)
+          )}
+        </div>
+      </main>
     </div>
   );
 };
@@ -28,7 +59,7 @@ Home.getLayout = (page) => {
 export const getServerSideProps = wrapper.getServerSideProps(
   (store) => async () => {
     const res = await get("players?per_page=10");
-    await store.dispatch(setTeams(res.data.data));
+    await store.dispatch(setPlayers(res.data.data));
     return {
       props: {},
     };
