a105aa1eccdb9c3ed1c90bfbf4898d69b8f1b85f
Thar Lin Shwe <albent2017@gmail.com>
2022-09-26 09:38:32 +0630

    Added Nav

---
+++ components/Nav.tsx
@@ -0,0 +1,34 @@
+import { NextPageWithLayout } from "types/page";
+import styles from "@/styles/nav.module.scss";
+import { useRouter } from "next/router";
+import { useDispatch, useSelector } from "react-redux";
+import { clearName, getUserName } from "@/store/features/authSlice";
+import Link from "next/link";
+
+import { BsPersonCircle } from "react-icons/bs";
+
+export const Nav: NextPageWithLayout = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const username = useSelector(getUserName);
+
+  function logout() {
+    //TODO: Implement logout function
+    dispatch(clearName);
+    router.push("/login");
+  }
+
+  return (
+    <div className={styles["nav-container"]}>
+      <Link href="/">Football team manager</Link>
+      <div>
+        <span className={styles["user-name"]}>
+          <BsPersonCircle /> {username}
+        </span>
+        <button className="btn" onClick={logout} arial-label="Logout">
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+};
