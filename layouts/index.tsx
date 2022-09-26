6e8bec3b5c6a0839ba75355d55b1dd0e0ccd84f5
Thar Lin Shwe <albent2017@gmail.com>
2022-09-26 09:39:45 +0630

    Added Nav & Footer to layout

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -1,12 +1,32 @@
-import { FC } from "react";
+import { Footer } from "@/components/Footer";
+import { Nav } from "@/components/Nav";
+import { getUserName } from "@/store/features/authSlice";
+import { useRouter } from "next/router";
+import { FC, useEffect } from "react";
 import { ReactNode } from "react";
+import { useSelector } from "react-redux";
 
 interface Props {
   children: ReactNode;
 }
 
 const Layout: FC<Props> = ({ children }) => {
-  return <div>{children}</div>;
+  const router = useRouter();
+  const username = useSelector(getUserName);
+
+  useEffect(() => {
+    if (username === "" || username === null) {
+      router.push("/login");
+    }
+  }, []);
+
+  return (
+    <div>
+      <Nav />
+      {children}
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
